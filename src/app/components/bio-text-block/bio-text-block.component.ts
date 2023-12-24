7d5b6cbeed66b26fc5967f181902dd7d5d193a9d
k1n4kut4 <recruitlukejones@gmail.com>
2023-12-24 03:50:38 +0000

    skeleton of components

---
+++ src/app/components/bio-text-block/bio-text-block.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bio-text-block',
+  standalone: true,
+  imports: [],
+  templateUrl: './bio-text-block.component.html',
+  styleUrl: './bio-text-block.component.sass'
+})
+export class BioTextBlockComponent {
+
+}
