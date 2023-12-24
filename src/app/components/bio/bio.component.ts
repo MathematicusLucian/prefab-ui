e7078e320ae2e108a6a85624fe5a5b8f49a49922
k1n4kut4 <recruitlukejones@gmail.com>
2023-12-24 07:51:36 +0000

    Bio photo. Text

--- src/app/components/bio/bio.component.ts
+++ src/app/components/bio/bio.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BioPhotoComponent } from '../bio-photo/bio-photo.component';
+import { BioTextBlockComponent } from '../bio-text-block/bio-text-block.component';
 
 @Component({
   selector: 'app-bio',
   standalone: true,
-  imports: [],
+  imports: [BioPhotoComponent, BioTextBlockComponent],
   templateUrl: './bio.component.html',
   styleUrl: './bio.component.sass'
 })
