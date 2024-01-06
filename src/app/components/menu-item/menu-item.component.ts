4cb2d451c39f8e52cb2e0cd72583b2c4cd5499f0
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-06 20:45:29 +0000

    moved logic for router to menu item

--- src/app/components/menu-item/menu-item.component.ts
+++ src/app/components/menu-item/menu-item.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-menu-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: './menu-item.component.html',
   styleUrl: './menu-item.component.sass'
 })
-export class MenuItemComponent {
+export class MenuItemComponent implements OnChanges {
+  @Input() title: string = "";
+  @Input() linkPath: string = "";
+  title$: any = [];
+  linkPath$: any = [];
 
+  ngOnChanges(changes: any) {
+    console.log('ABC', changes.linkPath.currentValue);
+    this.title$ = changes.title.currentValue;
+    this.linkPath$ = changes.linkPath.currentValue;
+  }
 }
