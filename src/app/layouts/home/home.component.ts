50c2e1e0c063ec8a563d495fd7111439e5f84e5c
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-06 21:59:48 +0000

    Re-utilised and implemented Text Heading component

--- src/app/layouts/home/home.component.ts
+++ src/app/layouts/home/home.component.ts
@@ -1,21 +1,14 @@
 import { Component } from '@angular/core';
-import { TextHeadingComponent } from '../../components/text-heading/text-heading.component';
 import { BioComponent } from '../../components/bio/bio.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { SkillsComponent } from '../../components/skills/skills.component';
-import { BlockExperienceComponent } from '../../components/block-experience/block-experience.component';
 import { BlogComponent } from '../blog/blog.component';
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { FlexLayoutServerModule } from '@angular/flex-layout/server';
-import { MatCardModule } from '@angular/material/card';
-import { MatPaginatorModule } from '@angular/material/paginator';
 import { CommonModule } from '@angular/common';
-import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ProjectsComponent, CardComponent, TextHeadingComponent, BioComponent, SkillsComponent, BlockExperienceComponent, BlogComponent, MatCardModule, MatPaginatorModule, FlexLayoutModule, FlexLayoutServerModule],
+  imports: [CommonModule, ProjectsComponent, BioComponent, SkillsComponent, BlogComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
