f85901a72793fa1ff3cefe9adb7d13b158072483
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-06 22:08:35 +0000

    Added lazy loading to all routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,12 @@
-import { Route, Routes } from '@angular/router';
-import { HomeComponent } from './layouts/home/home.component';
-import { BlogComponent } from './layouts/blog/blog.component';
-import { ResumeComponent } from './layouts/resume/resume.component';
+import { Route } from '@angular/router';
 
 export const APP_ROUTES: Route[] = [
-    { path: 'home', component: HomeComponent },
-    // { path: 'blog', component: BlogComponent },
+    { path: 'home', loadComponent: () => 
+        import('./layouts/home/home.component').then(m => m.HomeComponent)},
     { path: 'blog', loadComponent: () => 
         import('./layouts/blog/blog.component').then(m => m.BlogComponent)},
-    { path: 'cv', component: ResumeComponent },
+    { path: 'cv', loadComponent: () => 
+      import('./layouts/resume/resume.component').then(m => m.ResumeComponent)},
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', redirectTo: '/home' },
 ];
