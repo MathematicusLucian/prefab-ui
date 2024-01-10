b138557eed3e579589e91ed88ad63a0af58b4e3d
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-10 14:43:41 +0000

    created tagline

--- src/app/layouts/blog/blog.component.ts
+++ src/app/layouts/blog/blog.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BlogPostComponent } from '../../components/blog-post/blog-post.component';
 import { TextHeadingComponent } from '../../components/text-heading/text-heading.component';
+import { TaglineComponent } from '../../components/tagline/tagline.component';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [BlogPostComponent, TextHeadingComponent],
+  imports: [BlogPostComponent, TextHeadingComponent, TaglineComponent],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.sass'
 })
--- src/app/layouts/resume/resume.component.ts
+++ src/app/layouts/resume/resume.component.ts
@@ -4,11 +4,12 @@ import { SkillComponent } from '../../components/skill/skill.component';
 import { BioComponent } from '../../components/bio/bio.component';
 import { BlockExperienceComponent } from '../../components/block-experience/block-experience.component';
 import { BlogComponent } from '../blog/blog.component';
+import { TaglineComponent } from '../../components/tagline/tagline.component';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [TextHeadingComponent, BioComponent, SkillComponent, BlockExperienceComponent, BlogComponent],
+  imports: [TextHeadingComponent, BioComponent, TaglineComponent, SkillComponent, BlockExperienceComponent, BlogComponent],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.sass'
 })
