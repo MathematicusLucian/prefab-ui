27b3976d23dfa22ee1498397046550c404a515c5
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-10 14:53:17 +0000

    mobile menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuItemComponent } from '../menu-item/menu-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHamburger } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
 // const routerOptions: ExtraOptions = {
 //   scrollPositionRestoration: 'enabled',
 //   anchorScrolling: 'enabled',
@@ -10,12 +13,14 @@ import { MenuItemComponent } from '../menu-item/menu-item.component';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MenuItemComponent],
+  imports: [CommonModule, MenuItemComponent, FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass'
 })
 export class HeaderComponent {
-  HeaderTitle = "Luke Jones";
+  hamburgerOpen = false;
+  headerTitle = "Luke Jones";
+  faHamburger = faHamburger;
   menuItems = [
     {
       "title": "Home",
@@ -96,10 +101,12 @@ export class HeaderComponent {
     }
   ];
 
-  constructor() {
+  constructor(private router: Router) {
+    this.router.events.subscribe((val: any) => this.toggleHamburger());
   }
 
-  ngOnInit(): void {
+  toggleHamburger(): void {
+    this.hamburgerOpen = !this.hamburgerOpen;
   }
 
 }
