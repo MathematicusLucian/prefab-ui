12154cb4860f6f51532902f8a5f9eb0ba9c8782c
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-10 14:53:17 +0000

    Added Routes for Home and Blog; Imported Components to the Layouts

---
+++ src/app/components/text-heading/text-heading.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-text-heading',
+  standalone: true,
+  imports: [],
+  templateUrl: './text-heading.component.html',
+  styleUrl: './text-heading.component.sass'
+})
+export class TextHeadingComponent {
+  @Input() headingText: string | undefined;
+}
