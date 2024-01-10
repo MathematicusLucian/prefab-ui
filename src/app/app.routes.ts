21d7f0173b82a50d74e145d7a46d8aece10673f5
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-10 14:53:17 +0000

    Added default app route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { BlogComponent } from './layouts/blog/blog.component';
 import { ResumeComponent } from './layouts/resume/resume.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'blog', component: BlogComponent },
     { path: 'cv', component: ResumeComponent }
