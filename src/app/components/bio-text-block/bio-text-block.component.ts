714e48e50c9ce54b102dd3f502f22c29bff5ff27
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-10 14:53:17 +0000

    Re-utilised and implemented Text Heading component

--- src/app/components/bio-text-block/bio-text-block.component.ts
+++ src/app/components/bio-text-block/bio-text-block.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TextHeadingComponent } from '../text-heading/text-heading.component';
 
 @Component({
   selector: 'app-bio-text-block',
   standalone: true,
-  imports: [],
+  imports: [TextHeadingComponent],
   templateUrl: './bio-text-block.component.html',
   styleUrl: './bio-text-block.component.sass'
 })
--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import skillsData from '../../../assets/skills.json';
 import { BadgeComponent } from '../badge/badge.component';
+import { TextHeadingComponent } from '../text-heading/text-heading.component';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [CommonModule, BadgeComponent],
+  imports: [CommonModule, BadgeComponent, TextHeadingComponent],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.sass'
 })
