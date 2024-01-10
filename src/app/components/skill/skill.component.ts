6af2be713f08d92ff7dd0004ca74ae977c75590a
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-10 14:43:38 +0000

    logic and styling and json file for skills section

--- src/app/components/skill/skill.component.ts
+++ src/app/components/skill/skill.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
+import skillsData from './../../../assets/skills.json';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skill',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skill.component.html',
   styleUrl: './skill.component.sass'
 })
-export class SkillComponent {
+export class SkillComponent implements OnInit {
+  skillsData = skillsData;
 
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.changeDetectorRef.detectChanges();
+  }
+
+  ngOnDestroy() {
+  }
 }
