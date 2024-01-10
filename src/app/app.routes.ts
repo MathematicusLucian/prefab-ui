2463191552ee18f64623ca503a0e51b70200ac35
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-10 14:43:36 +0000

    Added Routes for Home and Blog; Imported Components to the Layouts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './layouts/home/home.component';
+import { BlogComponent } from './layouts/blog/blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'blog', component: BlogComponent }
+];
