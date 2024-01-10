2c3f800b41ba333420e03fc2bb40e38e5f19cab5
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-10 14:43:37 +0000

    setup magic grid on home component

--- src/app/layouts/home/home.component.ts
+++ src/app/layouts/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TextHeadingComponent } from '../../components/text-heading/text-heading.component';
 import { BioComponent } from '../../components/bio/bio.component';
 import { SkillComponent } from '../../components/skill/skill.component';
@@ -6,8 +6,9 @@ import { BlockExperienceComponent } from '../../components/block-experience/bloc
 import { BlogComponent } from '../blog/blog.component';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { FlexLayoutServerModule } from '@angular/flex-layout/server';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
+import MagicGrid from 'magic-grid';
 
 @Component({
   selector: 'app-home',
@@ -16,7 +17,23 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   gridColumns = 3;
   cards = [{},{},{},{},{}];
+
+  constructor() {}
+
+  ngOnInit() {
+    let magicGrid = new MagicGrid({
+      container: "#magic-grid", 
+      items: this.cards.length,
+      gutter: 3,
+      maxColumns: 5,
+      useMin: true,
+      useTransform: true,
+      animate: true, 
+    });
+    magicGrid.listen();
+    magicGrid.positionItems();
+  }
 }
