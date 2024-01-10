37dd5a0d3c431b35d364c8d7cc8603b4b364e2e2
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-10 14:53:17 +0000

    moved logic for router to menu item

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,26 +2,23 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
+// import { MatToolbarModule } from '@angular/material/toolbar';
+// import { MatTooltipModule } from '@angular/material/tooltip';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { FlexLayoutServerModule } from '@angular/flex-layout/server';
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { ExtraOptions, RouterModule, RouterOutlet, provideRouter } from '@angular/router';
-// import { APP_ROUTES } from '../../app.routes';
-// [provideRouter(routes), provideClientHydration(), provideAnimations()]
-const routerOptions: ExtraOptions = {
-  scrollPositionRestoration: 'enabled',
-  anchorScrolling: 'enabled',
-  scrollOffset: [0, 64],
-};
+import { RouterOutlet } from '@angular/router';
+import { MenuItemComponent } from '../menu-item/menu-item.component';
+// const routerOptions: ExtraOptions = {
+//   scrollPositionRestoration: 'enabled',
+//   anchorScrolling: 'enabled',
+//   scrollOffset: [0, 64],
+// };
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule, FontAwesomeModule, MatToolbarModule, MatTooltipModule, FlexLayoutModule, FlexLayoutServerModule],
-  // providers: [provideRouter(routes)],
+  imports: [CommonModule, MenuItemComponent, RouterOutlet, FontAwesomeModule, FlexLayoutModule, FlexLayoutServerModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass'
 })
@@ -30,6 +27,20 @@ export class HeaderComponent {
   faLinkedin = faLinkedin;
   faEnvelope = faEnvelope;
   HeaderTitle = "Luke Jones";
+  menuItems = [
+    {
+      "title": "Home",
+      "linkPath": "/home/" 
+    },
+    {
+      "title": "Blog",
+      "linkPath": "/blog/" 
+    },
+    {
+      "title": "CV/Resume",
+      "linkPath": "/cv/" 
+    }
+  ];
 
   constructor() {
   }
