f605fb3af66ef1d03303b8054e9d3ecc8e481507
k1n4kut4 <recruitlukejones@gmail.com>
2024-02-03 15:27:18 +0000

    added skills menu to home page summary section

--- src/app/layouts/home/home.component.ts
+++ src/app/layouts/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ComponentRef, OnInit, OnChanges, ViewChild, ViewContainerRef, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppValues } from '../../core/config/enums';
 import { TextHeadingComponent } from '../../components/text-heading/text-heading.component';
@@ -7,58 +7,72 @@ import { TaglineSmallComponent } from '../../components/tagline-small/tagline-sm
 import { SkillsComponent } from '../../components/skills/skills.component';
 import { MenuItemComponent } from '../../components/menu-item/menu-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faLinkedin, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faGlobe, faLocationPin, faBriefcase, faCode, faUserPlus, faFileLines } from '@fortawesome/free-solid-svg-icons';
 import { ButtonComponent } from '../../components/button/button.component';
 import { SiteGraphService } from '../../core/services/site-graph/site-graph.service';
+import { MenuBlockComponent } from '../../components/menu-block/menu-block.component';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, TextHeadingComponent, TaglineComponent, TaglineSmallComponent, ButtonComponent, MenuItemComponent, SkillsComponent, FontAwesomeModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.sass'
+  styleUrl: './home.component.sass',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit, OnChanges {
+  @ViewChild('vcr', { static: true, read: ViewContainerRef })
+  vcr!: ViewContainerRef;
+  componentFactoryItems: ComponentRef<MenuBlockComponent>[] = [];
+  cdr = inject(ChangeDetectorRef);
+  private linksMenuData$: BehaviorSubject<any> = new BehaviorSubject<any>({});
   appValues = AppValues;
-  menuRightItems = [
-    {
-      "title": null,
-      "icon": "faGithub",
-      "linkPath": "/github/",
-      "target": "_blank", 
-      "active": false
-    },{
-      "title": null,
-      "icon": "faLinkedin",
-      "linkPath": "/linkedin/",
-      "target": "_blank", 
-      "active": false
-    },{
-      "title": null,
-      "icon": "faEnvelope",
-      "linkPath": "/contact/",
-      "target": null, 
-      "active": false
-    }
-  ];
   faBriefcase = faBriefcase;
   faGlobe = faGlobe;
   faLocationPin = faLocationPin;
   faCode = faCode;
   faUserPlus = faUserPlus;
-  faFileLines = faFileLines
+  faFileLines = faFileLines;
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
+  faXTwitter = faXTwitter;
   blockName = "home";
   homeData$!: any;
+  linksMenuData!: any;
   skillsData$!: any;
   skillsCategoriesData$!: any;
 
   constructor(private siteGraphService: SiteGraphService) {}
 
   ngOnInit() {
- 
     // this.homeData$ = this.siteGraphService.fetchBlocks(this.blockName);
+    this.linksMenuData = this.siteGraphService.fetchBlocks('menu_links');
     this.skillsData$ = this.siteGraphService.fetchBlocks('tags');
     this.skillsCategoriesData$ = this.siteGraphService.fetchBlocks('categories');
+    this.renderDynamicComponents(null);
+  }
 
+  ngOnChanges() {
+    this.renderDynamicComponents(null);
+  }
+
+  renderDynamicComponents(component?: any) {
+    this.vcr.clear();
+    const componentFactoryItem2 = this.vcr.createComponent(MenuBlockComponent);
+    componentFactoryItem2.instance.menuData$ = this.linksMenuData;
+    componentFactoryItem2.instance.colorScheme = "light";
+    componentFactoryItem2.instance.isRightLeaning = "false";
+    this.componentFactoryItems.push(componentFactoryItem2);
+    this.cdr.detectChanges();
+  }
+
+  ngOnDestroy(): void {
+    for (const componentFactoryItem of this.componentFactoryItems) {
+      if (componentFactoryItem) {
+        componentFactoryItem.destroy();
+      }
+    }
   }
 }
